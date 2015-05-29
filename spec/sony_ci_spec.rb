1f2506071edf37b2bcaaf0794ac2705297b3a9fa
Chuck McCallum <chuck_mccallum@wgbh.org>
2015-05-29 18:37:50 -0400

    Reproducer for #2

--- spec/sony_ci_spec.rb
+++ spec/sony_ci_spec.rb
@@ -64,6 +64,12 @@ describe 'Sony Ci API' do
       it 'allows 6M files' do
         expect_big_upload(safe_ci, 6)
       end
+      xit 'allowd 701M files' do
+        # Reproducer for https://github.com/WGBH/sony-ci-api/issues/2
+        # Currently hitting a timeout:
+        # 408 REQUEST_TIMEOUT
+        expect_big_upload(safe_ci, 701)
+      end
     end
     
   end
